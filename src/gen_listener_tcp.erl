b881e6e470196e927de4f3bdbb99cf5e14d23eeb
Essien Ita Essien <essiene@gmail.com>
2009-01-13 13:44:49 +0100

    Redesign gen_listener_tcp to look more like a normal gen_server.
    With the new design, we have internal server states, and can
    respond to handle_{call,cast,info} and terminate and code_change
    messages.

--- src/gen_listener_tcp.erl
+++ src/gen_listener_tcp.erl
@@ -10,7 +10,16 @@
         start/4,
         start_link/3,
         start_link/4,
-        stop/1
+        call/2,
+        call/3,
+        multicall/2,
+        multicall/3,
+        multicall/4,
+        cast/2,
+        cast/3,
+        abcast/2,
+        abcast/3,
+        reply/2
     ]).
 
 
@@ -26,13 +35,20 @@
 -record(listener_state, {
         socket,
         acceptor,
-        mfa
+        mod,
+        mod_state
         }).
 
 
 behaviour_info(callbacks) ->
     [
-        {init, 1}
+        {init, 1},
+        {handle_accept, 2},
+        {handle_call, 3},
+        {handle_cast, 2},
+        {handle_info, 2},
+        {terminate, 2},
+        {code_change, 3}
     ];
 
 behaviour_info(_Other) ->
@@ -40,81 +56,145 @@ behaviour_info(_Other) ->
 
 
 start_link(Name, Module, InitArgs, Options) ->
-    gen_server:start_link(Name, ?MODULE, [{je_cb_module, Module} | InitArgs], Options).
+    gen_server:start_link(Name, ?MODULE, [{'__gen_listener_tcp_mod', Module} | InitArgs], Options).
 
 start_link(Module, InitArgs, Options) ->
-    gen_server:start_link(?MODULE, [{je_cb_module, Module} | InitArgs], Options).
+    gen_server:start_link(?MODULE, [{'__gen_listener_tcp_mod', Module} | InitArgs], Options).
 
 start(Name, Module, InitArgs, Options) ->
-    gen_server:start(Name, ?MODULE, [{je_cb_module, Module} | InitArgs], Options).
+    gen_server:start(Name, ?MODULE, [{'__gen_listener_tcp_mod', Module} | InitArgs], Options).
 
 start(Module, InitArgs, Options) ->
-    gen_server:start(?MODULE, [{je_cb_module, Module} | InitArgs], Options).
+    gen_server:start(?MODULE, [{'__gen_listener_tcp_mod', Module} | InitArgs], Options).
 
-stop(Name) ->
-    gen_server:cast(Name, stop).
+call(ServerRef, Request) ->
+    gen_server:call(ServerRef, Request).
 
+call(ServerRef, Request, Timeout) ->
+    gen_server:call(ServerRef, Request, Timeout).
+
+multicall(Name, Request) ->
+    gen_server:multicall(Name, Request).
+
+multicall(Nodes, Name, Request) ->
+    gen_server:multicall(Nodes, Name, Request).
+
+multicall(Nodes, Name, Request, Timeout) ->
+    gen_server:multicall(Nodes, Name, Request, Timeout).
+
+cast(ServerRef, Request) ->
+    gen_server:cast(ServerRef, Request).
+
+cast(ServerRef, Request, Timeout) ->
+    gen_server:cast(ServerRef, Request, Timeout).
+
+abcast(Name, Request) ->
+    gen_server:abcast(Name, Request).
+
+abcast(Nodes, Name, Request) ->
+    gen_server:abcast(Nodes, Name, Request).
+
+reply(Client, Reply) ->
+    gen_server:reply(Client, Reply).
 
 % gen_server callbacks
 
-init([{je_cb_module, Module} | InitArgs]) ->
+init([{'__gen_listener_tcp_mod', Module} | InitArgs]) ->
     process_flag(trap_exit, true),
 
     case Module:init(InitArgs) of
-        {ok, {Port, Options, Mfa}} ->
+        {ok, {Port, Options}, ModState} ->
             {ok, ListenSocket} = gen_tcp:listen(Port, Options),
 
             error_logger:info_report([listening_started, {port, Port}, {lsock, ListenSocket} | Options]), 
 
-            {ok, ListenerState} = create_acceptor(ListenSocket, Mfa),
-            {ok, ListenerState};
+            {ok, create_acceptor(ListenSocket, Module, ModState)};
         Other ->
             {stop, Other}
     end.
 
 
-handle_call(Request, _From, ListenerState) ->
-    {reply, {illegal, Request}, ListenerState}.
+handle_call(Request, From, #listener_state{mod=Module, mod_state=ModState}=St) ->
+    case Module:handle_call(Request, From, ModState) of 
+        {reply, Reply, NewModState} ->
+            {reply, Reply, St#listener_state{mod_state=NewModState}};
+        {reply, Reply, NewModState, hibernate} ->
+            {reply, Reply, St#listener_state{mod_state=NewModState}, hibernate};
+        {reply, Reply, NewModState, Timeout} ->
+            {reply, Reply, St#listener_state{mod_state=NewModState}, Timeout};
+        {noreply, NewModState} ->
+            {noreply, St#listener_state{mod_state=NewModState}};
+        {noreply, NewModState, hibernate} ->
+            {noreply, St#listener_state{mod_state=NewModState}, hibernate};
+        {noreply, NewModState, Timeout} ->
+            {noreply, St#listener_state{mod_state=NewModState}, Timeout};
+        {stop, Reason, NewModState} ->
+            {stop, Reason, St#listener_state{mod_state=NewModState}};
+        {stop, Reason, Reply, NewModState} ->
+            {stop, Reason, Reply, St#listener_state{mod_state=NewModState}}
+    end.
 
-handle_cast(stop, ListenerState) ->
-    {stop, normal, ListenerState};
+handle_cast(Request, #listener_state{mod=Module, mod_state=ModState}=St) ->
+    case Module:handle_cast(Request, ModState) of
+        {noreply, NewModState} ->
+            {noreply, St#listener_state{mod_state=NewModState}};
+        {noreply, NewModState, hibernate} ->
+            {noreply, St#listener_state{mod_state=NewModState}, hibernate};
+        {noreply, NewModState, Timeout} ->
+            {noreply, St#listener_state{mod_state=NewModState}, Timeout};
+        {stop, Reason, NewModState} ->
+            {stop, Reason, St#listener_state{mod_state=NewModState}}
+    end.
 
-handle_cast(_Request, ListenerState) ->
-    {noreply, ListenerState}.
 
-handle_info({inet_async, LSock, ARef, {ok, ClientSock}}, #listener_state{socket=LSock, acceptor=ARef}=ListenerState) ->
+handle_info({inet_async, LSock, ARef, {ok, ClientSock}}, #listener_state{socket=LSock, acceptor=ARef, mod=Module, mod_state=ModState}=St) ->
     error_logger:info_report([new_connection, {csock, ClientSock}, {lsock, LSock}, {async_ref, ARef}]),
+    patch_client_socket(ClientSock, LSock),
+
+    error_logger:info_report([handling_accept, {module, Module}, {module_state, ModState}]),
+
     try
-        patch_client_socket(ClientSock, LSock),
-        {Module, Function, Args} = ListenerState#listener_state.mfa,
-        NewArgs = [ClientSock | Args],
-
-        error_logger:info_report([calling_handler, {module, Module}, {function, Function}, {args, NewArgs}]),
-        {ok, _Pid} = apply(Module, Function, NewArgs)
+        case Module:handle_accept(ClientSock, ModState) of
+            {noreply, NewModState} ->
+                {noreply, create_acceptor(St#listener_state{mod_state=NewModState})};
+            {noreply, NewModState, hibernate} ->
+                {noreply, create_acceptor(St#listener_state{mod_state=NewModState}), hibernate};
+            {noreply, NewModState, Timeout} ->
+                {noreply, create_acceptor(St#listener_state{mod_state=NewModState}), Timeout};
+            {stop, Reason, NewModState} ->
+                {stop, Reason, create_acceptor(St#listener_state{mod_state=NewModState})}
+        end
     catch
-        Type:Exception -> 
-            error_logger:error_report({Type, Exception}),
-            gen_tcp:close(ClientSock)
-    end,
-
-
-    {ok, NewListenerState} = create_acceptor(ListenerState),
-    {noreply, NewListenerState};
+        Type:Exception ->
+            error_logger:error_report([gen_listener_tcp, {action, handle_accept}, {type, Type}, {exception, Exception}]),
+            gen_tcp:close(ClientSock),
+            {noreply, St}
+    end;
 
 handle_info({inet_async, LSock, ARef, Error}, #listener_state{socket=LSock, acceptor=ARef}=ListenerState) ->
     error_logger:error_report([acceptor_error, {reason, Error}, {lsock, LSock}, {async_ref, ARef}]),
     {stop, Error, ListenerState};
 
-handle_info(_Info, ListenerState) ->
-    {noreply, ListenerState}.
+handle_info(Info, #listener_state{mod=Module, mod_state=ModState}=St) ->
+    case Module:handle_info(Info, ModState) of
+        {noreply, NewModState} ->
+            {noreply, St#listener_state{mod_state=NewModState}};
+        {noreply, NewModState, hibernate} ->
+            {noreply, St#listener_state{mod_state=NewModState}, hibernate};
+        {noreply, NewModState, Timeout} ->
+            {noreply, St#listener_state{mod_state=NewModState}, Timeout};
+        {stop, Reason, NewModState} ->
+            {stop, Reason, St#listener_state{mod_state=NewModState}}
+    end.
 
-terminate(Reason, ListenerState) ->
+terminate(Reason, #listener_state{mod=Module, mod_state=ModState}=St) ->
     error_logger:info_report([listener_terminating, {reason, Reason}]),
-    gen_tcp:close(ListenerState#listener_state.socket),
-    ok.
+    gen_tcp:close(St#listener_state.socket),
+    Module:terminate(Reason, ModState).
 
-code_change(_OldVsn, ListenerState, _Extra) ->
-    {noreply, ListenerState}.
+code_change(OldVsn, #listener_state{mod=Module, mod_state=ModState}=St, Extra) ->
+    {ok, NewModState} = Module:code_change(OldVsn, ModState, Extra),
+    {ok, St#listener_state{mod_state=NewModState}}.
 
 
 % prim_inet imports
@@ -126,11 +206,11 @@ patch_client_socket(CSock, LSock) when is_port(CSock), is_port(LSock) ->
     ok = prim_inet:setopts(CSock, Opts),
     ok.
 
-create_acceptor(ListenerState) when is_record(ListenerState, listener_state) ->
-    create_acceptor(ListenerState#listener_state.socket, ListenerState#listener_state.mfa).
+create_acceptor(St) when is_record(St, listener_state) ->
+    create_acceptor(St#listener_state.socket, St#listener_state.mod, St#listener_state.mod_state).
 
-create_acceptor(ListenSocket, Mfa) when is_port(ListenSocket) ->
+create_acceptor(ListenSocket, Module, ModState) when is_port(ListenSocket) ->
     {ok, Ref} = prim_inet:async_accept(ListenSocket, -1), 
 
     error_logger:info_report(waiting_for_connection), 
-    {ok, #listener_state{socket=ListenSocket, acceptor=Ref, mfa=Mfa}}.
+    #listener_state{socket=ListenSocket, acceptor=Ref, mod=Module, mod_state=ModState}.
