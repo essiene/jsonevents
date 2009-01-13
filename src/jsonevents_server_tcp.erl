2fc6acd9bda63379fca59cfab67b98fd08e1213d
Essien Ita Essien <essiene@gmail.com>
2009-01-13 13:46:06 +0100

    Update jsonevents_server_tcp to work with the new gen_listener_tcp

--- src/jsonevents_server_tcp.erl
+++ src/jsonevents_server_tcp.erl
@@ -8,7 +8,13 @@
     ]).
 
 -export([
-        init/1
+        init/1,
+        handle_accept/2,
+        handle_call/3,
+        handle_cast/2,
+        handle_info/2,
+        terminate/2,
+        code_change/3
     ]).
 
 start() ->
@@ -18,21 +24,34 @@ start_link(Config) ->
     gen_listener_tcp:start_link({local, ?TCP_LISTENER}, ?MODULE, [Config], []).
 
 init([Config]) ->
-    {ok, 
-        {
-            Config:get(server.port.tcp, 8195), 
+    {
+        ok, 
+        {Config:get(server.port.tcp, 8195), 
             [ 
                 binary, 
                 inet, 
                 {active, false}, 
                 {backlog, Config:get(server.backlog, 10)},
                 {reuseaddr, true} 
-            ], 
-            
-            { 
-                jsonevents_session_sup, 
-                start_child, 
-                []
-            } 
-        }
+            ]},
+        nil
     }.
+
+handle_accept(Sock, State) -> 
+    jsonevents_session_sup:start_child(Sock),
+    {noreply, State}.
+
+handle_call(Request, _From, State) ->
+    {reply, {illegal_request, Request}, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
