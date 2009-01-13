5b892e0fb0e95aeb9883a53415f2be1dabad6a0d
Essien Ita Essien <essiene@gmail.com>
2009-01-13 13:46:53 +0100

    Update ssl listener to work with new gen_listener_tcp too

--- src/jsonevents_server_ssl.erl
+++ src/jsonevents_server_ssl.erl
@@ -4,13 +4,24 @@
 
 -export([
         start/0,
-        start_link/1,
-        upgrade_connection/2
+        start_link/1
     ]).
 
 -export([
-        init/1
+        init/1,
+        handle_accept/2,
+        handle_call/3,
+        handle_cast/2,
+        handle_info/2,
+        terminate/2,
+        code_change/3
     ]).
+
+-record(server_state, {
+        cacertfile,
+        certfile,
+        keyfile}).
+
 
 start() ->
     gen_listener_tcp:start({local, ?SSL_LISTENER}, ?MODULE, [erlcfg:new()], []).
@@ -28,28 +39,39 @@ init([Config]) ->
                 {active, false}, 
                 {backlog, Config:get(server.backlog, 10)},
                 {reuseaddr, true} 
-            ], 
-            
-            { 
-                ?MODULE,
-                upgrade_connection,
-                [Config]
-            } 
-        }
-    }.
+            ]}, 
+        #server_state{cacertfile = Config:get(server.ssl.cacerts, "/etc/jsonevents/cacerts.pem"), 
+                      certfile = Config:get(server.ssl.cert, "/etc/jsonevents/cert.pem"), 
+                      keyfile = Config:get(server.ssl.key, "/etc/jsonevents/key.pem")}
+      }.
 
-
-upgrade_connection(ClientSocket, Config) ->
-    error_logger:info_msg("Upgrading connection ~p to SSL~n", [ClientSocket]),
-    SslOpts = [
-        {cacertfile, Config:get(server.ssl.cacerts, "/etc/jsonevents/cacerts.pem")},
-        {certfile, Config:get(server.ssl.cert, "/etc/jsonevents/cert.pem")},
-        {keyfile, Config:get(server.ssl.key, "/etc/jsonevents/key.pem")}
+handle_accept(Sock, St) ->
+    error_logger:info_msg("Upgrading connection ~p to SSL~n", [Sock]),
+    SslOpts = [{cacertfile, St#server_state.cacertfile},
+        {certfile, St#server_state.certfile},
+        {keyfile, St#server_state.keyfile}
     ],
 
-    case ssl:ssl_accept(ClientSocket, SslOpts) of
+    case ssl:ssl_accept(Sock, SslOpts) of
         {ok, SslSock} -> 
-            jsonevents_session_sup:start_child(SslSock, ssl, ssl, ssl, ssl_closed, ssl_error);
+            jsonevents_session_sup:start_child(SslSock, ssl, ssl, ssl, ssl_closed, ssl_error),
+            {noreply, St};
         {error, Reason} ->
-            error_logger:error_report([jsonevents, {action, upgrade_connection}, {error, Reason}])
+            error_logger:error_report([jsonevents, {action, upgrade_connection}, {error, Reason}]),
+            {noreply, St}
     end.
+
+handle_call(Request, _From, State) ->
+    {reply, {illegal_request, Request}, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
